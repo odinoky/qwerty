ad6fa7f76f0095bff99f5b19942dde69728e986b
FREDOM1979 <5baxoff@gmail.com>
2022-07-08 05:55:14 +0600

    нгвгевге

--- connect/index.js
+++ connect/index.js
@@ -657,7 +657,3 @@ console.log(chalk.redBright(`Update ${__filename}`))
 delete require.cache[file]
 require(file)
 })
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
