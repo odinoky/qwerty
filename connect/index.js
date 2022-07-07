866ae062756905f965a3c16ee2caccfd5d84b296
FREDOM1979 <5baxoff@gmail.com>
2022-07-07 23:31:24 +0600

    акчыавя

--- connect/index.js
+++ connect/index.js
@@ -9,7 +9,8 @@
 
 
 require('../settings')
-const { default: makeWASocket, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto , sleep} = require("@adiwajshing/baileys")
+const { WAConnection } = require("@adiwajshing/baileys") 
+const { default: makeWASocket, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
 const pino = require('pino')
 const fs = require('fs')
