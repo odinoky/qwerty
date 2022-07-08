5ca8bb968b4ceb48cf6eb6bb4de8ae89b7503fe6
FREDOM1979 <5baxoff@gmail.com>
2022-07-08 09:05:11 +0600

    гдш7аеш7акш

--- connect/index.js
+++ connect/index.js
@@ -72,7 +72,7 @@ if (mek.key && mek.key.remoteJid === 'status@broadcast') return
 if (!ichi.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
 if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
 m = smsg(ichi, mek, store) 
-if ("../command/ichi.js")(ichi, m, chatUpdate, store) 
+require("../command/ichi.js")(ichi, m, chatUpdate, store)
 } catch (err) {
 console.log(err)
 }
